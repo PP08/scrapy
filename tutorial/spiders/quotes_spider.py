63b84f7fe56e88b25f76e0af9bfead15f7ff6d76
phucphuong <phucphuong1293@gmail.com>
2017-11-11 16:37:00 +0700

    update parsing functions

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -1,14 +1,17 @@
-import scrapy
-from datetime import datetime
-from tutorial.items import MacappItem
-from slugify import slugify
 import re
+from datetime import datetime
+import scrapy
+from slugify import slugify
+from tutorial.items import MacappItem
+
+
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
         'https://cmacapps.com',
     ]
     count = 1
+
     def parse(self, response):
         self.count += 1
         for article in response.xpath("//article[contains(@class, 'post')]"):
@@ -19,8 +22,6 @@ class QuotesSpider(scrapy.Spider):
                     icon = icon.split(', ')[-1].split(' ')[0]
                 else:
                     icon = article.xpath("./figure/a/img/@src").extract_first()
-                # if icon:
-                #     item["icon"] = icon.split(', ')[-1].split(' ')[0]
                 item["icon"] = icon
                 date_upload = article.xpath("./div/div/div[1]/time/@datetime").extract_first()
                 date_upload = datetime.strptime(date_upload, "%Y-%m-%d")
@@ -29,8 +30,7 @@ class QuotesSpider(scrapy.Spider):
                 item["slug"] = slugify(item["name"])
                 link = article.xpath("./div/div/h2/a/@href").extract_first()
                 item["category"] = article.xpath("./div/div/p/span[2]/a/text()").extract()
-                print(item)
-                # yield scrapy.Request(url=link, callback=self.parse_detail, meta={'item': item})
+                yield scrapy.Request(url=link, callback=self.parse_detail, meta={'item': item})
             except:
                 pass
         # next_page = 'https://cmacapps.com/page/{}/'.format(self.count)
@@ -38,10 +38,10 @@ class QuotesSpider(scrapy.Spider):
         #     yield scrapy.Request(url=next_page, callback=self.parse)
 
     def parse_detail(self, response):
-
         item = response.meta['item']
         if 'wpappbox' in response.text:
-            screenshots = response.xpath("//div[contains(@class, 'wpappbox')]/div[@class='screenshots']/div/ul/li/img/@src").extract()
+            screenshots = response.xpath(
+                "//div[contains(@class, 'wpappbox')]/div[@class='screenshots']/div/ul/li/img/@src").extract()
             if len(screenshots) > 0:
                 screenshots = [x[2:] for x in screenshots]
                 item["screenshots"] = screenshots
@@ -62,8 +62,8 @@ class QuotesSpider(scrapy.Spider):
                 item["screenshots"] = screenshots.split(', ')[-1].split(' ')[0]
             description = response.xpath("//div[@id='description']").extract_first()
             if description:
-                description = re.sub(r'https://cdn.cmacapps.com', 'http://127.0.0.1:8080/upload/q_100/https://cdn.cmacapps.com', description)
+                description = re.sub(r'https://cdn.cmacapps.com',
+                                     'http://127.0.0.1:8080/upload/q_100/https://cdn.cmacapps.com', description)
             item["description"] = description
             item["download_link"] = response.xpath("//p[contains(@class, 'p1')]/a/@href").extract()
             yield item
-
